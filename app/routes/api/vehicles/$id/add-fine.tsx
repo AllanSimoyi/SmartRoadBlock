d20ee217180e6f616d989758108a12bafc15e266
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2022-11-22 15:17:41 +0200

    Replace badrequest

--- app/routes/api/vehicles/$id/add-fine.tsx
+++ app/routes/api/vehicles/$id/add-fine.tsx
@@ -2,12 +2,21 @@ import type { ActionArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { z } from "zod";
 import { prisma } from "~/db.server";
-import { badRequest, PositiveDecimalSchema, PositiveIntSchema } from "~/lib/core.validations";
+import type { inferSafeParseErrors } from "~/lib/core.validations";
+import { PositiveDecimalSchema, PositiveIntSchema } from "~/lib/core.validations";
 import { FALLBACK_ERROR_MESSAGE } from "~/lib/errors";
 
 const Schema = z.object({
   amount: PositiveDecimalSchema,
 });
+
+type FieldErrors = inferSafeParseErrors<typeof Schema>;
+
+function fieldErrorsToString (fieldErrors: FieldErrors) {
+  const { amount } = fieldErrors;
+  const array = [amount?.join(", ") || ""];
+  return array.filter(el => el).join(", ");
+}
 
 export async function action ({ request, params }: ActionArgs) {
   try {
@@ -23,7 +32,13 @@ export async function action ({ request, params }: ActionArgs) {
     const result = await Schema.safeParseAsync(fields);
     if (!result.success) {
       const { formErrors, fieldErrors } = result.error.flatten();
-      return badRequest({ fields, fieldErrors, formError: formErrors.join(", ") });
+      const strFieldErrors = fieldErrorsToString(fieldErrors);
+      const strFormErrors = formErrors.join(", ") || "";
+      const errorMessage = [strFieldErrors, strFormErrors]
+        .filter(el => el)
+        .join(", ");
+      console.log("errorMessage: ", errorMessage);
+      return json({ errorMessage }, { status: 200 });
     }
     const { amount } = result.data;
 
